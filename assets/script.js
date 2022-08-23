390431a73f186ed92d899640e878dd7a4b86fccf
Will D <will.dean0103@gmail.com>
2022-08-23 17:01:18 -0400

    tried to get timer to work on the page

--- assets/script.js
+++ assets/script.js
@@ -7,6 +7,11 @@ var submitScoreBtn = document.querySelector("#submitBtn");
 var quizTitle = document.querySelector("#quizTitle");
 var questionsEl = document.querySelector("#questions");
 var titleEl = document.querySelector("#title");
+var yourInitials = document.querySelector('#initials');
+var yourScores = document.querySelector('#yourScores');
+var timeEl = document.querySelector('.timeLeft');
+var secondsLeft = 60;
+
 
 var questions = [
     {
@@ -50,14 +55,29 @@ function currentSelection() {
         quizEl.style.display = 'none';
         endEl.style.display = 'block';
     }
+    if (state === 'scores') {
+
+    }
 }
 
+timeEl.textContent = secondsLeft
+function startTimer() {
+    var timeInterval = setInterval(function () {
+        secondsLeft--;
+        timeEl.textContent = secondsLeft;
+        if (secondsLeft === 0) {
+            clearInterval(timeInterval)
+        }
+
+    }, 1000);
+};
 
 currentSelection()
 function nextQuestion(event) {
-    questionOrder ++;
-    questionsEl.innerHTML = null 
-    if(questionOrder < questions.length) {
+    checkAnswer(event.target.textContent)
+    questionOrder++;
+    questionsEl.innerHTML = null
+    if (questionOrder < questions.length) {
         displayQuestion()
     } else {
         state = 'end'
@@ -70,32 +90,53 @@ function nextQuestion(event) {
 function displayQuestion() {
     state = 'quiz';
     var questionDisplay = questions[questionOrder];
-    var questionH2 = document. createElement('h2');
-        questionH2.textContent = questions[questionOrder].question
-        questionsEl.appendChild(questionH2)
-    for(i = 0; i < questions[questionOrder].answers.length; i ++) {
-        
+    var questionH2 = document.createElement('h2');
+    questionH2.textContent = questions[questionOrder].question
+    questionsEl.append(questionH2)
+    startTimer();
+    for (i = 0; i < questions[questionOrder].answers.length; i++) {
+
         buttonEl = document.createElement('button')
         buttonEl.textContent = questions[questionOrder].answers[i];
         buttonEl.addEventListener('click', nextQuestion)
-        questionsEl.appendChild(buttonEl);
+        questionsEl.append(buttonEl);
 
     }
 }
 
-startBtn.addEventListener("click", function (event) {
+
+startBtn.addEventListener("click", function () {
     state = 'quiz';
-    currentSelection()
+    currentSelection();
+    
 });
 
 submitScoreBtn.addEventListener("click", function () {
+    state = 'scores'
+    yourInitials = yourInitials.value
+    localStorage.setItem('initials', yourInitials)
+    currentSelection()
+
 
 });
-
+// checks if answer is right or wrong and displays a message that says correct or wrong
 function checkAnswer(answers) {
-    if(answers == question[questionOrder].correct) {
-        alert('correct')
-    } else {
-        alert('wrong')
+    if (answers == questions[questionOrder].correct) {
+        alert('Correct!')
     }
-}
+    else {
+        alert('Wrong!')
+    }
+};
+
+function highScores(event) {
+    yourInitials = ''
+    for (var i = 0; i < initialsList.length; i++) {
+        initial = yourInitials[i]
+        var li = document.createElement('li')
+        li.textContent = initial
+        li.setAttribute('data-index', i)
+        initialsList.appendchild[i]
+
+    }
+};
